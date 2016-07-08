2447933c90f6de3f0da3fa290c3c253cca29237d
Hendrik Beijeman <hendrik.beijeman@eu.omron.com>
2016-07-08 08:30:11 +0200

    Cleanup, added documentation.

--- src/main.c
+++ src/main.c
@@ -23,8 +23,6 @@ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdint.h>
 #include <stddef.h>
 
-//#define UNITTEST
-
 #include "stm32f0xx.h"
 #include "stm32f0xx_rcc.h"
 #include "stm32f0xx_tim.h"
@@ -36,7 +34,6 @@ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 /**
  *          Pin Mappings
  *
- *          NEW:
  *
  *          PA3 -> P1   D0
  *          PA4 -> P9   D10
@@ -66,8 +63,8 @@ typedef enum {
 } T_SHUTTER_PHASE;
 
 #define TB                      (10)                // timebase divisor
-#define TRIG2_P7_SAMPLE_WAIT    (72000/TB)  // time to wait before sampling P7
-#define TRIG2_P7_SAMPLE_HOLD    (130000/TB)  // time to wait before sampling P7
+#define TRIG2_P7_SAMPLE_WAIT    (72000/TB)          // time to wait before sampling P7
+#define TRIG2_P7_SAMPLE_HOLD    (130000/TB)         // time to stop sampling P7
 
 #define P1_SET      (volatile uint16_t*)&GPIOA->BSRR,   (1<<3)
 #define P1_CLR      (volatile uint16_t*)&GPIOA->BRR,    (1<<3)
@@ -86,13 +83,14 @@ typedef enum {
 
 static inline uint32_t TIMESTAMP(void) { return TIM3->CNT; }
 
+/** The I/O pin timing structure */
 typedef struct {
-    uint32_t            trig;
-    volatile uint16_t*  io_reg;
-    uint16_t            state;
+    uint32_t            trig;       //!< Trigger time for this signal
+    volatile uint16_t*  io_reg;     //!< The I/O port to toggle
+    uint16_t            state;      //!< The I/O pin to toggle
 } T_SHUTTER_TIMING;
 
-// Trigger: D8/digital input (110ms) (time-base in 2us (divide by /2))
+/** Shutter-open timings */
 static const T_SHUTTER_TIMING timing_open[] =
 {
      {25850/TB,        P9_CLR},
@@ -102,12 +100,11 @@ static const T_SHUTTER_TIMING timing_open[] =
      {42000/TB,        P1_SET},
      {62900/TB,        P10_CLR},
      {71400/TB,        P12_SET},
-     //{110500/TB,       P1_CLR},         // <<<< Problem with short mode / conflict
 
      {0, 0, 0},       // END
 };
 
-// Trigger: D8/digital input (18ms) (time-base in 2us (divide by /TB))
+/** Shutter-close timings for long mode */
 static const T_SHUTTER_TIMING timing_close_long[] =
 {
      {200/TB,           P1_SET},
@@ -122,25 +119,26 @@ static const T_SHUTTER_TIMING timing_close_long[] =
      {0,0, 0},         // END
 };
 
-// Trigger: D8/digital input (18ms) (time-base in 2us (divide by /TB))
+/** Shutter-close timings for short mode */
 static const T_SHUTTER_TIMING timing_close_short[] =
 {
-     {2800/TB,          P1_CLR},    // OK
-     {4300/TB,          P1_SET},    // OK
-     {5600/TB,          P1_CLR},    // OK
-     {31600/TB,         P12_CLR},   // OK
-     {67800/TB,         P8_SET},    // OK
-     {104000/TB,        P8_CLR},    // OK
-     {105000/TB,        P9_SET},    // OK
+     {2800/TB,          P1_CLR},
+     {4300/TB,          P1_SET},
+     {5600/TB,          P1_CLR},
+     {31600/TB,         P12_CLR},
+     {67800/TB,         P8_SET},
+     {104000/TB,        P8_CLR},
+     {105000/TB,        P9_SET},
 
      {0,0, 0},         // END
 };
 
+/** Main mangement structure */
 typedef struct S_SHUTTER {
-    uint8_t                 active;
-    T_SHUTTER_PHASE         phase;
-    T_SHUTTER_TIMING const* timing;
-    void (*state)(struct S_SHUTTER* const sh, T_TRIGGER const action);
+    uint8_t                 active;     //!< Shutter logic active 
+    T_SHUTTER_PHASE         phase;      //!< The shutter phase?
+    T_SHUTTER_TIMING const* timing;     //!< The active timing values
+    void (*state)(struct S_SHUTTER* const sh, T_TRIGGER const action);  //!< The state handler for events
 } T_SHUTTER;
 
 static uint32_t time = 0;
@@ -159,6 +157,14 @@ static void stop_timebase(void)
     TIM_Cmd(TIM3, DISABLE);
 }
 
+/**
+ * @brief High accuracy time-base overflow handler
+ *
+ * The high accuracy timer can overflow. Thats why it is started/stopped on-demand 
+ * for the shutter logic. The overflow happens +- 0.6s, but the activation triggers
+ * last much shorter than this - 130ms at most. If the timer is not stopped, some
+ * critical error occured and we just lock up.
+ */
 static void timer_base_overflow(T_SHUTTER* const sh)
 {
     if (sh->active) {
@@ -167,6 +173,9 @@ static void timer_base_overflow(T_SHUTTER* const sh)
     }
 }
 
+/**
+ * @brief Handle events when shutter is in the close state
+ */
 static void handler_state_close(T_SHUTTER* const sh, T_TRIGGER const action)
 {
     __disable_irq();
@@ -185,6 +194,9 @@ static void handler_state_close(T_SHUTTER* const sh, T_TRIGGER const action)
     __enable_irq();
 }
 
+/**
+ * @brief Handle events when the shutter is in the open state
+ */
 static void handler_state_open(T_SHUTTER* const sh, T_TRIGGER const action)
 {
     __disable_irq();
@@ -213,6 +225,9 @@ static void handler_state_open(T_SHUTTER* const sh, T_TRIGGER const action)
     __enable_irq();
 }
 
+/**
+ * @brief Handle events when the shutter is in the idle state
+ */
 static void handler_state_idle(T_SHUTTER* const sh, T_TRIGGER const action)
 {
     __disable_irq();
@@ -228,23 +243,21 @@ static void handler_state_idle(T_SHUTTER* const sh, T_TRIGGER const action)
     __enable_irq();
 }
 
-// Called every 100us for state-keeping
+/**
+ * @brief Called every 100us or so for timekeeping
+ */
 static void timer_do(T_SHUTTER* const sh)
 {
     if (!sh->active)
         return;
 
-    // Handle signals
+    /* Check for the I/O timings and set them */
     if (sh->timing && sh->timing->trig && (TIMESTAMP() >= sh->timing->trig)) {
         *sh->timing->io_reg = sh->timing->state;
         sh->timing++;
-
-        if (sh->timing && sh->timing->trig == 0) {
-            volatile uint32_t x =0 ;
-            x++;
-        }
     }
 
+    /* Handle state dependent timer events */
     sh->state(sh, TIMER);
 }
 
@@ -318,8 +331,12 @@ static void init_peripherals(void)
     EXTI_Init(&EXTI_InitStruct);
 }
 
+/**
+ * @brief Set the I/O pin default values
+ */
 static void pins_startup(void)
 {
+    /* Important! P9 must be active by default! */
     GPIO_WriteBit(GPIOA, GPIO_Pin_4, Bit_SET);
 
     GPIO_WriteBit(GPIOA, GPIO_Pin_3, Bit_RESET);
@@ -346,7 +363,9 @@ void main(void)
     FOR_EVER {}
 }
 
-// Trigger input
+/**
+ * @brief Handle the P5 IRQ (rising & falling edges)
+ */
 __attribute__((__interrupt__)) void irq_trigger(void)
 {
     if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
@@ -362,7 +381,9 @@ __attribute__((__interrupt__)) void irq_trigger(void)
     EXTI_ClearITPendingBit(EXTI_Line0);
 }
 
-// 100us TIMER
+/**
+ * @brief Handle the application timer
+ */
 __attribute__((__interrupt__)) void irq_tim2(void)
 {
     if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
@@ -372,19 +393,30 @@ __attribute__((__interrupt__)) void irq_tim2(void)
     }
 }
 
-// 10us TIMER
+/**
+ * @brief Handle the one-shot high-accuracy timebase overflow.
+ *
+ * This should never fire!!
+ */
 __attribute__((__interrupt__)) void irq_tim3(void)
 {
     if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) {
         TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-        // handle time-base overflow event
-
         timer_base_overflow(&shutter);
     }
 }
 
+/**
+ * @brief Handle 1ms SYSTICK
+ *
+ * This should never fire!!
+ */
 __attribute__((__interrupt__)) void  irq_systick(void)
 {
+    /* OK; systick could've been used instead of the HighRes timebase. 
+     * In fact, both the timer and the high-res timebase can be dealt
+     * with purely from within the systick if set to 100us. 
+     * Accuracy is enough - but the current implementation works. */
     time++;
 }
 
